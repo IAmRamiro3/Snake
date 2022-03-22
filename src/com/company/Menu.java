6deb13eb7ef8c632730d4d6a3b6db00317d16ba2
IAmRamiro3 <ramironunezsanchez3@gmail.com>
2022-03-22 16:26:29 -0700

    Integracion de boton temprana

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -1,13 +1,30 @@
 package com.company;
 
-import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.*;
 
-public class Menu extends JPanel{
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.plaf.DimensionUIResource;
+
+import org.w3c.dom.events.Event;
+
+public class Menu extends JPanel implements ActionListener {
 
     static final int Ancho = 200;
     static final int Alto = 200;
+    public JButton boton = new JButton();
 
     Menu(){
-        //this.setPreferredSize(new Dimension(Ancho, Alto));
+        this.setPreferredSize(new DimensionUIResource(Ancho, Alto));
+        this.setBackground(Color.DARK_GRAY);
+        this.setFocusable(true);
+
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+    }
+
 }
