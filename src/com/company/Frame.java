6e2a21cbf05bcd2f4ccda08401a3201b21e3b5bc
IAmRamiro3 <ramironunezsanchez3@gmail.com>
2022-03-22 17:21:39 -0700

    Ouch

--- src/com/company/Frame.java
+++ src/com/company/Frame.java
@@ -1,17 +1,30 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Frame extends JFrame {
+public class Frame extends JFrame implements ActionListener {
+
+    JButton boton = new JButton("Hola");
 
     Frame() {
-
         this.add(new Panel());
+        boton.setBounds(100,100,50,50);
+        this.add(boton);
         this.setTitle("Snake");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
         this.setVisible(true);
         this.setLocationRelativeTo(null);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == boton){
+            new Panel();
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
     }
 }
--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -1,30 +1,36 @@
 package com.company;
 
-import java.awt.Color;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.plaf.DimensionUIResource;
 
-import org.w3c.dom.events.Event;
-
-public class Menu extends JPanel implements ActionListener {
+public class Menu extends JFrame implements ActionListener {
 
     static final int Ancho = 200;
     static final int Alto = 200;
-    public JButton boton = new JButton();
+    public JButton boton = new JButton("Iniciar");
 
     Menu(){
         this.setPreferredSize(new DimensionUIResource(Ancho, Alto));
         this.setBackground(Color.DARK_GRAY);
         this.setFocusable(true);
+        boton.setBounds(100,100,100,100);
+        this.add(boton);
+        boton.addActionListener(this);
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e){
+        if (e.getSource() == boton){
+            //this.add(new Panel());
+            //System.exit(0);
+            new Panel();
 
+        }
     }
 
 }
